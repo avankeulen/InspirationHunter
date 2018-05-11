1a62953ebb0162b0755d5e9f6cd3388f26f8952c
avankeulen <alexander.van_keulen@hotmail.com>
2018-05-11 16:21:59 +0200

    Profile Pic fix & Delete btn fix
    Profielfoto’s worden nu verandert van naam en worden verplaatst naar de
    map ‘uploads/avatar’. Delete button zit nu in posts.inc.php

--- settings.php
+++ settings.php
@@ -17,14 +17,26 @@ if(!empty($_POST)){
     $username = $_SESSION['username'];
     $newusername = $_POST['username'];
     $bio = $_POST['bio'];
+
+    $temp = explode(".", $_FILES['profilePicture']['name']);
+    $newfilename = round(microtime(true)) . '.' . end($temp);
+    $foto = $newfilename;
+
     $user_img = $foto;
     $newpassword = "";
     if(!empty($_POST['newPassword'])) {
         $newpassword = $_POST['newPassword'];
     }
 
+    define ('SITE_ROOT', realpath(dirname(__FILE__)));
+
     $result = $u->changeSettings($username, $newusername, $bio, $user_img, $newpassword);
     if($result === true){
+
+        $destFile = __DIR__ . '/images/uploads/avatar/' . $user_img;
+        move_uploaded_file($_FILES['profilePicture']['tmp_name'], $destFile);
+        chmod($destFile, 0666);
+
         //echo "De gegevens zijn succesvol aangepast, gelieve terug in te loggen met de nieuwe gegevens";
         session_destroy();
         header('location: login.php');
@@ -55,13 +67,14 @@ if( $statement->rowCount() > 0){
 <body>
 <?php include_once ('inc/nav.inc.php'); ?>
 
-<h1> Pas account aan:  </h1>
+<section class="content">
+<h1> Edit account:  </h1>
 
 <div style="margin:20px 10px 10px 10px; padding:25px;border-radius:7px;background-color:rgba(93,180,205,0.25);box-shadow: 0 2px 3px rgba(0,0,0,.16);color:#0781ad;border:solid 1px #0781ad;opacity:0.66;">
     Warning: Editing your account will require us to automatically log you in again! 
 </div>
 
-    <section class="login-form-wrap3" class="content" >
+    <section class="login-form-wrap3">
 
         <form class="password-form" method="POST" action="" enctype="multipart/form-data" id="upload-form">
             <label>username :
@@ -72,7 +85,16 @@ if( $statement->rowCount() > 0){
             </label><br />
 
             <label for="profilePicture">Mijn profielfoto</label>
-            <input type="file" name="profilePicture" id="profilePicture" accept="image/gif, image/jpeg, image/png, image/jpg"><br />
+            <div id="prev-div">
+                <figure class="<?php $_POST['filter'] ?>">
+                    <img id="img-prev" src="#" alt="uploaded image" />
+                </figure>
+                <br>
+            </div>
+            <input type="file" name="profilePicture" id="profilePicture" accept="image/gif, image/jpeg, image/png, image/jpg" onchange="readURL(this);"><br />
+
+
+
             <img id="imgPreview" src="img.php" alt="" style="height: 200px;" />
             </label><br />
 
@@ -81,7 +103,35 @@ if( $statement->rowCount() > 0){
             </label><br /><br />
             <input class="buttonReset" type="submit" value="Pas gegevens aan">
         </form>
-    </section> 
+    </section>
+
+</section>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+    $('#prev-div').hide();
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#prev-div').show();
+                $('#img-prev').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    getLocationName();
+
+    function showFilter() {
+        var x = document.getElementById("upload-filter").value;
+        document.getElementsByTagName("figure")[0].setAttribute("class", x);
+    }
+
+</script>
 
 
 </body>
